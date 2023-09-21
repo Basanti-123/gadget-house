8e56877b49afcdb49e6a635bf19112b0ec7a5ac4
BasantiNagari <basantinagari23@gmail.com>
2023-09-21 20:24:26 +0545

    email verification during user authentication

--- server/modules/users/user.model.js
+++ server/modules/users/user.model.js
@@ -12,11 +12,18 @@ const userSchema = new Schema({
     required: "Email address is required",
     validate: [validateEmail, "Please fill a valid email address"],
   },
+  isEmailVerified: {type: Boolean, default: false},
   password: {
     type: String,
     required: true,
   },
+
+  isActive: {type: Boolean, default: true},
   ...commonSchema,
 });
+
+
+
+
 
 module.exports = model("User", userSchema);
